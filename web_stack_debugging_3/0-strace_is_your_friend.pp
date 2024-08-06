27e98458853670577f8ca7707248aaf7d894da80
Relebohile.P <r.pheko@alustudent.com>
2024-08-06 09:04:34 -0700

    fix Apache 500 error

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fixes Apache 500 error
-
+# Fixes apache 500 error
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
+}
